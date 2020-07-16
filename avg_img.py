80df6abcf093a0dee12026204e1fb796c39e9d47
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2020-07-16 16:46:40 +0900

    avg_img and img_scrape combined in main.py

--- avg_img.py
+++ avg_img.py
@@ -2,38 +2,36 @@ from os import listdir
 from PIL import Image
 import numpy as np
 
-# desired output image size
-x = 400
-y = 400
-# directory to the folder with images
-directory = 'images'
-# output name
-output = 'avg.jpg'
+def avg_img(x=400, y=400, directory = 'images', output = 'avg.jpg'):
+    """
+    the function averages the pictures in the 'directory' and
+    the output is saved as 'output' with pixels 'x' by 'y'
+    """
 
 
-# black (blank) image data XxY(RGB) in numpy array format
-master_data = np.zeros((y,x,3), int)
-# counts the total number of images processed
-count = 0
+    # black (blank) image data XxY(RGB) in numpy array format
+    master_data = np.zeros((y,x,3), int)
+    # counts the total number of images processed
+    count = 0
 
 
-# go through all images in a directory
-for filename in listdir(directory):
-    count += 1
-    # load each image 
-    img = Image.open(f'{directory}/{filename}')
-    # resize image to fit the master_data
-    img_resized = img.resize((x,y))
-    # turns image to numpy array
-    img_data = np.asarray(img_resized)
-    # sums all images
-    master_data += img_data
+    # go through all images in a directory
+    for filename in listdir(directory):
+        count += 1
+        # load each image 
+        img = Image.open(f'{directory}/{filename}')
+        # resize image to fit the master_data
+        img_resized = img.resize((x,y))
+        # turns image to numpy array
+        img_data = np.asarray(img_resized)
+        # sums all images
+        master_data += img_data
 
 
-# average the data
-master_data = master_data//count
-# turn numpy data into image
-master_image = Image.fromarray(master_data.astype("uint8"))
+    # average the data
+    master_data = master_data//count
+    # turn numpy data into image
+    master_image = Image.fromarray(master_data.astype("uint8"))
 
-# saves the image to the directory
-master_image.save(output)
+    # saves the image to the directory
+    master_image.save(output)
---
+++ main.py
@@ -0,0 +1,6 @@
+from avg_img import avg_img
+from img_scrape import scrape
+
+scrape("actor")
+
+avg_img(x=100, y=100)
