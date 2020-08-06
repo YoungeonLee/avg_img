98648b8c08a3c4a392c13da2a5f96e2e1101441c
YoungeonLee <58812396+YoungeonLee@users.noreply.github.com>
2020-08-06 17:15:44 +0900

    Revert "added functionality to classify the images"
    This reverts commit 2fdbf3247ddb8db60478d229712640a6855d2178.

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from avg_img import avg_img, avg_img_with_different_amounts
 from google_image_scrape import scrape
-from image_classifier import classify_images
-"""
-try:
-    scrape('fire', float("inf"), warn = False)
-finally:
-    avg_img_with_different_amounts(1080, 1080, output = "shoes")
-    
-"""
-#avg_img(1080,1080, output = "shoesfin")
 
-#scrape('pepper', 100, warn = False)
-classify_images(32, 32)
+#scrape('shoes', 100, warn = False)  
+
+avg_img_with_different_amounts(1080, 1080, output = "shoes")
+    
